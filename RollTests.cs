ab2dda45bb4efc8f6cf921a07af41b4d4299ceab
Adrian Unruh <unruha@uw.edu>
2021-03-17 01:21:52 -0700

    added test for random role and score calc

--- RollTests.cs
+++ RollTests.cs
@@ -1,16 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GreedKata.Models;
+using System;
 
 namespace RollTest
 {
     [TestClass]
     public class RollTests
     {
+        // output a random role and its score
         [TestMethod]
-        public void testRollDiceRandom()
+        public void testRandomAndScoreCalc()
         {
             Roll roll = new Roll();
+
             roll.rollDiceRandom();
+            roll.outputDiceValues();
+
+            Console.WriteLine();
+
+            roll.calculateScore();
+            roll.displayScore();
         }
+
     }
 }
